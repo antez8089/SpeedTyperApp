8b233bd9e65e8740fde9acd645126d0a13e3ee24
Igor Szablowski <iszablow@gmail.com>
2024-03-27 19:42:15 +0100

    User model and repository

---
+++ server/speedtyping/src/main/java/com/pw/speedtyping/database/models/User.java
@@ -0,0 +1,66 @@
+package com.pw.speedtyping.database.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    @Column(unique = true)
+    @Email
+    private String email;
+
+    @NotNull
+    private String password;
+
+    public User() {};
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
