2f9463e91eb7cb9883d4a9a9f6e2d9802dcdb212
Igor Szablowski <iszablow@gmail.com>
2024-03-28 01:03:36 +0100

    User registration and database connection

--- server/speedtyping/src/main/java/com/pw/speedtyping/controllers/AuthController.java
+++ server/speedtyping/src/main/java/com/pw/speedtyping/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package com.pw.speedtyping.controllers;
 
+import com.pw.speedtyping.database.models.User;
+import com.pw.speedtyping.database.repository.UserRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,13 +16,39 @@ import java.util.Map;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final UserRepository userRepository;
+
+    public AuthController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @PostMapping("/sign-up")
-    public ResponseEntity<Map<String, Object>> signUp(@RequestBody Map<String, Object> userData) {
+    public ResponseEntity<Map<String, Object>> signUp(@RequestBody User user) {
+        Map<String, Object> response = new HashMap<>();
 
-        //Here will be user validation
-        //Return json format object about registration
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            response.put("registered", false);
+            response.put("message", "user with this username already exists");
+            return ResponseEntity.ok(response);
+        }
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            response.put("registered", false);
+            response.put("message", "user with this username already exists");
+            return ResponseEntity.ok(response);
+        }
+        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
 
-        return ResponseEntity.ok(userData);
+        try {
+            userRepository.save(user);
+        } catch (Exception e) {
+            response.put("registered", false);
+            response.put("message", "invalid email");
+            return ResponseEntity.ok(response);
+        }
+        response.put("registered", true);
+        response.put("message", "user registered");
+        return ResponseEntity.ok(response);
+
     }
 
 }
