c0b874ecefef16afb526b5c2d81aee8f263035c5
“[firstname <01178222@pw.edu.pl>
2024-03-28 19:42:36 +0100

    chenged colours and added some borders to make it look more smooth

--- client/speedtyping/src/components/UserOptions.js
+++ client/speedtyping/src/components/UserOptions.js
@@ -1,7 +1,14 @@
 function UserOptions() {
     return (
-      <div >
-        cos
+      <div className="user-boxer">
+            <div className="two-boxer">
+                <div>Change name</div>
+                <div>Bchange password</div>
+            </div>
+            <div className="two-boxer">
+                <div>Change email</div>
+                <div>change profile picture</div>
+            </div>
       </div>
       );
   }
--- client/speedtyping/src/pages/keyboardPage.js
+++ client/speedtyping/src/pages/keyboardPage.js
@@ -6,7 +6,7 @@ function KeyboardPage() {
   return (
     <div className='container'>
       <div className="text-container">
-        <h4>Let's Type</h4>
+        <h4>Let's Type!</h4>
         <h3>Tutaj bedzie tekst sie zmieniał</h3>
         <div className='text' contentEditable></div>
       </div>
