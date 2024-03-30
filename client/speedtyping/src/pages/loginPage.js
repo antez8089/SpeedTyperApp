29200c705db55888959897c9ecc4e29a4fc01a97
“[firstname <01178222@pw.edu.pl>
2024-03-30 12:45:07 +0100

    login

--- client/speedtyping/src/pages/loginPage.js
+++ client/speedtyping/src/pages/loginPage.js
@@ -1,18 +1,53 @@
+import React, { useState } from 'react';
+import api from '../api/axiosConfig.js'
 
 
 function LoginPage() {
 
-    const onSubmit = (data) => {
-        console.log(data);
+    const getTokenFromCookie = () => {
+        const cookieToken = document.cookie.split(';').find(cookie => cookie.trim().startsWith('token='));
+        if (cookieToken) {
+            return cookieToken.split('=')[1];
+        }
+        return null;
     }
 
+    const setTokenInCookie = (tokenValue) => {
+        document.cookie = `token=${tokenValue}; Path=/; HttpOnly`;
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        const userJsonData = formToJSON(new FormData(e.target));
+
+        //odbieranie danych z form
+        const response = api.post('/auth/sign-in', userJsonData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (response.data.get('logged_in')) {
+            const token = response.data.token;
+            setTokenInCookie(token);
+            console.log(token);
+        } else {
+            console.log("login failed");
+        }
+
+    };
+        
+    const handleLogout = () => {
+        setTokenInCookie(null);
+    };
 
     return (
         <div className="flex justify-center">
             <form className="flex flex-col ">
                 <div>
-                    <label htmlFor='username'>Username</label>
-                    <input type='username' name='username' placeholder="username"/>
+                    <label htmlFor='Email'>Email</label>
+                    <input type='email' name='email' placeholder="email"/>
                 </div>
 
                 <div>
@@ -20,7 +55,7 @@ function LoginPage() {
                     <input type='password' name='password'/>
                 </div>
 
-                <button onClick={onSubmit}>Login</button>
+                <button onClick={handleLogin}>Login</button>
             </form>
         </div>
     )
