50a06fadd7a10ce94c813f7ea6848e348dcec4c2
Igor Szablowski <iszablow@gmail.com>
2024-04-16 23:02:07 +0200

    writing in client

--- client/speedtyping/src/pages/keyboardPage.js
+++ client/speedtyping/src/pages/keyboardPage.js
@@ -1,14 +1,33 @@
+import { useState, useEffect } from "react";
 import Keyboard from "../components/Keyboard";
+import TypingInput from "../components/TypingInput";
+import api from"../api/axiosConfig"
 
 
 function KeyboardPage() {
+
+  const [words, setWords] = useState([]);
+
+  const getWords = async () => {
+    const response = await api.get("/words", {
+        params: {
+            num: 10
+        }
+    });
+    setWords(response.data)
+  }
+
+  useEffect(() => {
+    getWords()
+  }, [])
   
   return (
     <div className='container'>
       <div className="text-container">
         <h4>Let's Type!</h4>
-        <h3>Tutaj bedzie tekst sie zmieniał</h3>
-        <div className='text' contentEditable></div>
+
+        <TypingInput words={words}></TypingInput>
+        
       </div>
       <Keyboard></Keyboard>
     </div>
